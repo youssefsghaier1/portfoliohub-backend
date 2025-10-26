43afc9b798a2313c83c95fdffa65fb4e0e506a6e
youssefsghaier1 <sghaier.youssef@esprit.tn>
2025-10-26 01:35:17 +0100

    initial spring boot project SETUP

---
+++ backend/src/test/java/com/portfoliohub/backend/PortfoliohubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliohub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliohubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
